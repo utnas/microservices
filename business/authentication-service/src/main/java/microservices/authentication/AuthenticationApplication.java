f55618ded08ce555763bc1d19feb800090acfb74
dnsewolo <nsewolo@gmail.com>
2015-10-02 15:26:21 -0400

    Authentication service added and recognized as a service

--- business/authentication-service/src/main/java/microservices/authentication/AuthenticationApplication.java
+++ business/authentication-service/src/main/java/microservices/authentication/AuthenticationApplication.java
@@ -3,10 +3,17 @@ package microservices.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAuthorizationServer
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthenticationApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
     }
